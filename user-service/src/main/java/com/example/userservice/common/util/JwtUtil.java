c8dbc6abafda4c20b72524b68eed0e5c304ee9ec
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-06-12 11:13:12 +0900

    feat: 로그인 성공 후 token 발급

---
+++ user-service/src/main/java/com/example/userservice/common/util/JwtUtil.java
@@ -0,0 +1,44 @@
+package com.example.userservice.common.util;
+
+import static com.example.userservice.common.constants.jwt.TokenType.ACCESS_TOKEN;
+import static com.example.userservice.common.constants.jwt.TokenType.REFRESH_TOKEN;
+
+import com.example.userservice.common.constants.jwt.TokenType;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtUtil {
+
+    private final Environment env;
+
+    public String issueAccessToken(String userId) {
+        return Jwts.builder()
+                .subject(userId)
+                .claim("token-type", ACCESS_TOKEN.getName())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN.getExpirationTime()))
+                .signWith(Keys.hmacShaKeyFor(env.getProperty("jwt.secret").getBytes(StandardCharsets.UTF_8)))
+                .compact();
+    }
+
+    public String issueRefreshToken() {
+        return Jwts.builder()
+                .claim("token-type", REFRESH_TOKEN.getName())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + REFRESH_TOKEN.getExpirationTime()))
+                .signWith(Keys.hmacShaKeyFor(env.getProperty("jwt.secret").getBytes(StandardCharsets.UTF_8)))
+                .compact();
+    }
+
+}
