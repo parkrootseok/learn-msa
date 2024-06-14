12a6a02daef035b17c81446dd32652e95b94f129
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-06-14 16:05:18 +0900

    feat: active 설정을 통해 개발 환경에 맞는  yml 파일 주입

--- user-service/src/main/java/com/example/userservice/domain/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/domain/controller/UserController.java
@@ -34,6 +34,7 @@ public class UserController {
 
         StringBuilder sb = new StringBuilder();
         sb.append("It's working in user service. </br>");
+        sb.append("active profile: " + env.getProperty("profile.name")).append("</br>");
         sb.append("PORT(local.server.port): " + env.getProperty("local.server.port")).append("</br>");
         sb.append("PORT(server.port): " + env.getProperty("server.port")).append("</br>");
         sb.append("token secret: " + env.getProperty("jwt.secret")).append("</br>");
