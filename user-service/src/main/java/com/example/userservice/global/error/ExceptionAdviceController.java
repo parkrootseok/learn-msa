98a1fbf8638f49c383cb50f5e75c4a7e4cd55279
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-06-19 22:56:54 +0900

    feat: Exception Handling

---
+++ user-service/src/main/java/com/example/userservice/global/error/ExceptionAdviceController.java
@@ -0,0 +1,24 @@
+package com.example.userservice.global.error;
+
+import feign.FeignException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionAdviceController {
+
+    @ExceptionHandler({FeignException.class})
+    protected ResponseEntity handleFeignException(FeignException exception) {
+
+        return new ResponseEntity<>(
+                exception.getMessage(),
+                HttpStatus.valueOf(exception.status())
+        );
+
+    }
+
+
+
+}
