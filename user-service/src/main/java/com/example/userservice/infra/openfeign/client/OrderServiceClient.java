7ffbec104325daa697d79f0d7ad6a9b5110fd6b5
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-06-19 22:57:25 +0900

    fix: 잘못된 요청 uri 수정

--- user-service/src/main/java/com/example/userservice/infra/openfeign/client/OrderServiceClient.java
+++ user-service/src/main/java/com/example/userservice/infra/openfeign/client/OrderServiceClient.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "order-service")
+@FeignClient("order-service")
 public interface OrderServiceClient {
 
-    @GetMapping("/order-service/{userId}/orders-ng")
+    @GetMapping("/order-service/{userId}/orders")
     List<GetOrderResponse> getOrders(@PathVariable("userId") String userId);
 
 }
