1564143c11865a3981912fbad4d91969feba375a
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-06-19 21:50:22 +0900

    feat: 사용자에 대한 주문 정보 가져오기 (OpenFeign)

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,6 +19,11 @@ import org.springframework.web.client.RestTemplate;
  *  - 'spring-cloud-starter-netflix-eureka-server' 의존성을 추가한 경우 어노테이션을 사용하지 않아도 자동 등록
  */
 @EnableDiscoveryClient
+/**
+ *  [ @EnableFeignClient ]
+ *  - OpenFeign을 사용하기 위한 어노테이션
+ */
+@EnableFeignClients
 @SpringBootApplication
 public class UserServiceApplication {
 
